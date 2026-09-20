76358a7ae69189e4818110de4d2c7d547a1dd9d9
Arda Efe Ozbayrak <ardaefeozbayrak@gmail.com>
2026-09-20 15:48:40 +0300

    fix(gradle): add compose compiler plugin for Kotlin 2.0+ compatibility

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.protobuf)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
@@ -43,11 +44,6 @@ android {
 
     kotlinOptions {
         jvmTarget = "21"
-    }
-
-    // Compose derleyici sürümü (Kotlin 2.1+ ile uyumlu)
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 }
 
